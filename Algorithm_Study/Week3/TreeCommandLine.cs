ced7bde2af4c419ed11e6809157fea282d277ef5
hwang105814 <hwang105814@gmail.com>
2024-10-17 15:02:53 +0900

    TreeCommandLine

---
+++ Algorithm_Study/Week3/TreeCommandLine.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_
+{
+    internal class CommandLine
+    {
+        Tree<string> tree;
+        TreeNode<string> root;
+        TreeNode<string> current;
+
+        public CommandLine()
+        {
+            tree = new Tree<string>("root");
+            root = tree.Root;
+            current = root;
+
+            while (true)
+            {
+                Console.Write(current.Data + " > ");
+                string cmd = Console.ReadLine();
+
+                string[] parts = cmd.Split(' ');
+                switch (parts[0])
+                {
+                    case "mk":
+                        current.AddChild(new TreeNode<string>(parts[1]));
+                        break;
+                    case "print":
+                        tree.Print();
+                        break;
+                    case "cd":
+                        TreeNode<string> child = current.FindChild(parts[1]);
+                        if (parts[1] == "..")
+                        {
+                            TreeNode<string> parent = current.FindParent(root);
+                            if (parent == null)
+                            {
+                                Console.WriteLine("Already root dic");
+                            }
+                            else
+                            {
+                                current = parent;
+                            }
+                        }
+                        else
+                        {
+                            if (child != null)
+                            {
+                                current = child;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Directory not found.");
+                            }
+                        }
+                        break;
+                    case "ls":
+                        current.PrintChildren();
+                        break;
+                    case "rm":
+                        TreeNode<string> child2 = current.FindChild(parts[1]);
+                        if(child2 != null)
+                        {
+                            current.Children.Remove(child2);
+                            Console.WriteLine("Delete Success");
+                        }
+                        else
+                        {
+                            Console.WriteLine("There's no Data");
+                        }
+                        break;
+                }
+                if (parts[0] == "exit")
+                {
+                    break;
+                }
+            }
+        }
+        public static void Start()
+        {
+            new CommandLine();
+        }
+    }
+}
